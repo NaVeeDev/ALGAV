f934a517ebf5db9ae3894c3f73314663dd440571
yvanprt <yvan.parent@etu.sorbonne-universite.fr>
2025-10-22 17:10:11 +0200

    test de la fonction ecriture

--- test_files/test_utils.ml
+++ test_files/test_utils.ml
@@ -60,56 +60,113 @@ let create_binary_file (file_name : string) (list : int list) : unit =
   in
   loop list
 
+let test_file = "test_files/ressources"
+
+
 let test_lecture_ () =
   
   let content = [] in 
-  let file= "test_files/ressources/test_lecture_empty.bin" in 
+  let file= test_file^"/test_lecture_empty.bin" in 
   create_binary_file file content;
   Printf.printf "test_lecture_empty :\n";
   Utils.lecture file;
   print_newline ();
 
   let content = [0] in 
-  let file= "test_files/ressources/test_lecture_0.bin" in 
+  let file= test_file^"/test_lecture_0.bin" in 
   create_binary_file file content;
   Printf.printf "test_lecture_0 :\n   0 0 0 0 0 0 0 0\n   ";
   Utils.lecture file;
   print_newline ();
 
   let content = [1] in 
-  let file= "test_files/ressources/test_lecture_1.bin" in 
+  let file= test_file^"/test_lecture_1.bin" in 
   create_binary_file file content;
   Printf.printf "test_lecture_1 :\n   0 0 0 0 0 0 0 1\n   ";
   Utils.lecture file;
   print_newline ();
 
   let content = [130] in 
-  let file = "test_files/ressources/test_lecture_2.bin" in 
+  let file = test_file^"/test_lecture_2.bin" in 
   create_binary_file file content;
   Printf.printf "test_lecture_2 :\n   1 0 0 0 0 0 1 0\n   ";
   Utils.lecture file;
   print_newline ();
 
   let content = [130; 1] in 
-  let file = "test_files/ressources/test_lecture_3.bin" in 
+  let file = test_file^"/test_lecture_3.bin" in 
   create_binary_file file content;
   Printf.printf "test_lecture_3 :\n   1 0 0 0 0 0 1 0 0 0 0 0 0 0 0 1\n   ";
   Utils.lecture file;
   print_newline ();
 
   let content = [9; 42; 243; 87; 0; 32] in 
-  let file = "test_files/ressources/test_lecture_4.bin" in 
+  let file = test_file^"/test_lecture_4.bin" in 
   create_binary_file file content;
   Printf.printf "test_lecture_4 :\n   0 0 0 0 1 0 0 1 0 0 1 0 1 0 1 0 1 1 1 1 0 0 1 1 0 1 0 1 0 1 1 1 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0\n   ";
   Utils.lecture file;
   print_newline ()
 
+
+
+let test_ecriture_ () = 
+  let file = test_file^"/test_ecriture_empty.txt" in
+  let output = test_file^"/test_ecriture_empty.bin" in 
+  Printf.printf "test_ecriture_empty :\n   ";
+  Utils.ecriture file output;
+  Utils.lecture output;
+  print_newline ();
+
+  let file = test_file^"/test_ecriture_0.txt" in
+  let output = test_file^"/test_ecriture_0.bin" in 
+  Printf.printf "test_ecriture_0 :\n   0\n   ";
+  Utils.ecriture file output;
+  Utils.lecture output;
+  print_newline ();
+
+  let file = test_file^"/test_ecriture_1.txt" in
+  let output = test_file^"/test_ecriture_1.bin" in 
+  Printf.printf "test_ecriture_1 :\n   0 1\n   ";
+  Utils.ecriture file output;
+  Utils.lecture output;
+  print_newline ();
+
+  let file = test_file^"/test_ecriture_2.txt" in
+  let output = test_file^"/test_ecriture_2.bin" in 
+  Printf.printf "test_ecriture_2 :\n   0 0 0 1 1 1 1\n   ";
+  Utils.ecriture file output;
+  Utils.lecture output;
+  print_newline ();
+
+  let file = test_file^"/test_ecriture_3.txt" in
+  let output = test_file^"/test_ecriture_3.bin" in 
+  Printf.printf "test_ecriture_3 :\n   1 1 1 1 1 1 1 1\n   ";
+  Utils.ecriture file output;
+  Utils.lecture output;
+  print_newline ();
+
+  let file = test_file^"/test_ecriture_4.txt" in
+  let output = test_file^"/test_ecriture_4.bin" in 
+  Printf.printf "test_ecriture_4 :\n   1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0\n   ";
+  Utils.ecriture file output;
+  Utils.lecture output;
+  print_newline ();
+
+  let file = test_file^"/test_ecriture_5.txt" in
+  let output = test_file^"/test_ecriture_5.bin" in 
+  Printf.printf "test_ecriture_5 :\n   0 1 0 1 0 1 1 0 1 1 0 1 0 1 0\n   ";
+  Utils.ecriture file output;
+  Utils.lecture output;
+  print_newline ();
+;;
+
 (* Appeler cette fonction pour executer les tests *)
 let test_utils_ () =
   Printf.printf "\n___ Lancement des tests de test_utils.ml ___\n";
   try 
-    test_nth_bit_ ();
-    test_lecture_ ();
+    test_nth_bit_  ();
+    test_lecture_  ();
+    test_ecriture_ ();
     Printf.printf "___ Tous les tests de test_utils.ml sont passés ___\n"
   with e -> Printf.printf "_Un test de test_utils.ml n'est pas passé : \n%s\n" (Printexc.to_string e);
 ;;
