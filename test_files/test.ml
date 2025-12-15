7ce12b5e17814c29272c1a58f635f04ea949b4c6
yvanprt <yvan.parent@etu.sorbonne-universite.fr>
2025-12-15 17:51:08 +0100

    mini fix

--- test_files/test.ml
+++ test_files/test.ml
@@ -1,5 +1,5 @@
 let test_ () =
-  (* Test_utils.test_utils_ (); *)
+  Test_utils.test_utils_ ();
   Test_primitive.test_primitive_ ();
   Test_compression.test_compression_ ();
   Test_decompression.test_decompression_ ()
